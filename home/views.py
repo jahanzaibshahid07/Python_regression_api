e1e2354fb2de328b3db7af97338aef1fe45e26b2
jahanzaibshahid <jahanzaibshahid07@gmail.com>
2020-12-15 15:44:24 +0500

    last git

--- home/views.py
+++ home/views.py
@@ -7,19 +7,20 @@ from sklearn.linear_model import LinearRegression
 # y(cord) = y(intercept) + bx
 
 def Table(request): 
-    df = pd.read_csv('E:/predicted.csv') 
+    df = pd.read_csv('E:/Sred/predicted.csv') 
     geeks_object = df.to_html() 
     # # return HttpResponse(geeks_object)
     reg=LinearRegression()
     reg.fit(df[['Date']],df.Price)
-    df=pd.read_csv("E:/preloc.csv")
+    df=pd.read_csv("E:/Sred/preloc.csv")
     b1=df.to_html()
     p=reg.predict(df)
     df['Price']=p
     b3=df.to_html()
     pri={b3}
-    mak=df.to_csv('E:/sred.csv')
-    return HttpResponse(true)
+    mak=df.to_csv('E:/Sred/sred.csv')
+    x =  '{ "status": true }'
+    return HttpResponse(x)
     # return render(request,'index.html')
     
     # b3 = {geeks_object,b1,pre}
